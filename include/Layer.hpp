aa5c55c5343fb5521c5454d8258f49f2c06336c5
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2018-01-15 19:27:34 +0530

    Matrixifying functions added.

--- include/Layer.hpp
+++ include/Layer.hpp
@@ -9,6 +9,11 @@ class Layer {
 public:
   // constructor
   Layer(int size);
+  void setVal(int i, double v);
+
+  Matrix* matrixifyVals();
+  Matrix* matrixifyActivatedVals();
+  Matrix* matrixifyDerivedVals();
 private:
   int size;
   vector<Neuron*> neurons;
--- src/Layer.cpp
+++ src/Layer.cpp
@@ -9,3 +9,28 @@ Layer::Layer(int size) {
     this->neurons.push_back(n);
   }
 }
+
+void Layer::setVal(int i, double v) {
+  this->neurons.at(i).setVal(v);
+}
+
+Matrix* Layer::matrixifyVals() {
+  Matrix* m = new Matrix(1, this->neurons.size(), false);
+  for (int i = 0;  i < neurons.size(); i++) {
+    m->setValue(0, i, this->neurons.at(i)->getVal())
+  }
+}
+
+Matrix* Layer::matrixifyActivatedVals() {
+  Matrix* m = new Matrix(1, this->neurons.size(), false);
+  for (int i = 0;  i < neurons.size(); i++) {
+    m->setValue(0, i, this->neurons.at(i)->getActivatedVal())
+  }
+}
+
+Matrix* Layer::matrixifyDerivedVals() {
+  Matrix* m = new Matrix(1, this->neurons.size(), false);
+  for (int i = 0;  i < neurons.size(); i++) {
+    m->setValue(0, i, this->neurons.at(i)->getDerivedVal())
+  }
+}
